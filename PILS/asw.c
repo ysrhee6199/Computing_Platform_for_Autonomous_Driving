d6d670de20661557a5b07d5de4ca5de50e5b3e63
ysrhee6199 <yongse6199@gmail.com>
2022-11-03 03:25:03 +0900

    PILS  FIX

--- PILS/asw.c
+++ PILS/asw.c
@@ -1,14 +1,20 @@
 #include "ee.h"
 #include "Arduino.h"
 #include "serial.h"
+#include "Sense.h"
+#include "Compute.h"
+#include "Actuate.h"/* Model's header file */
+#include "rtwtypes.h"
+
 
 void mdelay(unsigned long delay_ms);
 
 void StartupHook(void)
 {
 	/* write below your code */
-
-
+	Sense_initialize();
+	Compute_initialize();
+	Actuate_initialize();
                /*end*/
 
     printfSerial("\nOS Begins... ", 0ul);
@@ -25,6 +31,25 @@ ISR2(TimerISR)
 
 /* write below your code */
 
+TASK(Task_Sense) {
+	 Sense_step();
+	 Compute_U.in = Sense_Y.out;
+	 printfSerial(":\t%f\n",  Sense_Y.out);
+	 TerminateTask();
+};
+
+TASK(Task_Compute) {
+	Compute_step();
+	Actuate_U.in = Compute_Y.out;
+	printfSerial(":\t\t-> %f\n", Compute_Y.out);
+	TerminateTask();
+};
+
+TASK(Task_Actuate) {
+	Actuate_step();
+	printfSerial(":\t\t->\t\t-> %f\n", Actuate_Y.out);
+	TerminateTask();
+};
 
 
         /*end*/
